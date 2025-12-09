bd2b30d25272f1ca8f6b809eb7eab6ec974a3803
alishukur2 <yaalishukur@gmail.com>
2025-12-09 09:33:43 +0400

    .env and firebase updated

--- src/firebase.js
+++ src/firebase.js
@@ -16,13 +16,13 @@ const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID;
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "MEASUREMENT_ID"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
 // Initialize Firebase
